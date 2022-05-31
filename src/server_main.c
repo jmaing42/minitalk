7635d2d5a742566c39a75da656cac78b6ed0cc0d
맹주영 <mjy9088@naver.com>
2022-06-01 04:00:54 +0900

    add variable timeout

--- src/server_main.c
+++ src/server_main.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 02:24:27 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/06/01 03:35:01 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/06/01 04:00:15 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#include "ft_exit.h"
 #include "ft_io.h"
+#include "ft_lib.h"
 #include "ft_memory.h"
+#include "ft_cstring.h"
+#include "ft_exit.h"
+
+#define ENVP_TIMEOUT_START "TIMEOUT="
+#define DEFAULT_TIMEOUT 420420
 
 t_context	*c(void)
 {
@@ -26,15 +31,24 @@ t_context	*c(void)
 	return (&context);
 }
 
-static void	init_context(void)
+static void	init_context(char **environ)
 {
 	c()->sessions = new_ft_simple_map_static(sizeof(pid_t));
 	if (!c()->sessions)
 		ft_exit(EXIT_FAILURE);
-	c()->timeout = 60000;
+	c()->timeout = DEFAULT_TIMEOUT;
 	c()->head = NULL;
 	c()->tail = NULL;
 	c()->busy = false;
+	environ--;
+	while (*++environ)
+	{
+		if (ft_cstring_starts_with(*environ, ENVP_TIMEOUT_START))
+		{
+			c()->timeout = ft_atoi(*environ + sizeof(ENVP_TIMEOUT_START) - 1);
+			break ;
+		}
+	}
 }
 
 static void	set_signal_handler(void)
@@ -68,12 +82,14 @@ static void	main_loop(void)
 	}
 }
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
+	(void)argc;
+	(void)argv;
 	ft_put_string(STDOUT_FILENO, "Server pid is ");
 	ft_put_number(STDOUT_FILENO, getpid());
 	ft_put_string(STDOUT_FILENO, ". enjoy!\n");
-	init_context();
+	init_context(envp);
 	set_signal_handler();
 	main_loop();
 	return (EXIT_FAILURE);
