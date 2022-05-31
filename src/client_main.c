76486e9c21b55ed6c47429427a546d808bdffa44
맹주영 <mjy9088@naver.com>
2022-06-01 03:57:06 +0900

    refactor client

--- src/client_main.c
+++ src/client_main.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 02:22:31 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/06/01 02:48:38 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/06/01 03:56:41 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#include "ft_io.h"
 #include "ft_lib.h"
 #include "ft_memory.h"
 #include "ft_cstring.h"
@@ -23,7 +22,7 @@
 #define ENVP_TIMEOUT_START "TIMEOUT="
 #define DEFAULT_TIMEOUT 42042
 
-static sig_atomic_t	g_ack;
+static sig_atomic_t	g_ack_received;
 
 static void	init_context(t_context *context, const char *pid, const char *str)
 {
@@ -39,13 +38,14 @@ static void	init_context(t_context *context, const char *pid, const char *str)
 
 static void	set_ack_true(int signal)
 {
-	if (signal == SIGUSR1)
-		g_ack = 1;
+	(void)signal;
+	g_ack_received = 1;
 }
 
-static void	set_signal_handler(void)
+static void	timeout_exit(int signal)
 {
-	signal(SIGUSR1, set_ack_true);
+	(void)signal;
+	exit(EXIT_FAILURE);
 }
 
 static useconds_t	parse_timeout(char **environ)
@@ -61,20 +61,21 @@ int	main(int argc, char **argv, char **envp)
 {
 	const useconds_t	timeout = parse_timeout(envp);
 	t_context			context;
-	int					signal;
+	int					signal_to_send;
 
 	if (argc != 3)
 		return (EXIT_FAILURE);
 	init_context(&context, argv[1], argv[2]);
-	set_signal_handler();
-	g_ack = 0;
+	signal(SIGUSR1, set_ack_true);
+	signal(SIGUSR2, timeout_exit);
+	g_ack_received = 0;
 	while (true)
 	{
-		signal = SIGUSR1;
-		if (handler(&context, !!g_ack))
-			signal = SIGUSR2;
-		g_ack = 0;
-		if (kill(context.server, signal))
+		signal_to_send = SIGUSR1;
+		if (handler(&context, !!g_ack_received))
+			signal_to_send = SIGUSR2;
+		g_ack_received = 0;
+		if (kill(context.server, signal_to_send))
 			exit(EXIT_FAILURE);
 		usleep(timeout);
 	}
